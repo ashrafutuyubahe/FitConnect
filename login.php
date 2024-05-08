02ed58ab0dee919a840fed577e6c70fd4cd22531
Ashrafu <ashraftuyubahe001@gmail.com>
2024-05-08 21:53:52 +0200

    adding login and register

---
+++ login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login</title>
+    <link rel="stylesheet" href="styles/login.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <div class="image">
+      <img src="images/login.png" alt="people" />
+    </div>
+
+    <form action="" method="post">
+      <div class="container">
+        <div>
+          <h1 class="header">Login</h1>
+        </div>
+        <div class="sub-header">
+          <h2>Welcome back to your account</h2>
+        </div>
+        <div>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email or Phone number"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+          />
+        </div>
+
+        <div>
+          <input type="checkbox" name="remember" id="" placeholder="remember" /> Remember me
+          <a href="#"> Forgot password?</a>
+        </div>
+        <div>
+          <button type="submit" class="sign-up-btn">Login</button>
+        </div>
+        <p>OR</p>
+        <div>
+          <button type="submit" class="google">Login with Google</button>
+        </div>
+        <p class="text-bottom">
+          Don't have an account ? <a href="sign_up.html">Sign up</a>
+        </p>
+      </div>
+    </form>
+  </body>
+</html>
+
+<?php
+
+
+?>
