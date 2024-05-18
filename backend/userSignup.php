6ec25190e9d3e51d7ddf5d5d6091eb4e0ef4d765
Ashrafu <ashraftuyubahe001@gmail.com>
2024-05-18 08:27:07 +0200

    adding security scripts

--- backend/userSignup.php
+++ backend/userSignup.php
@@ -7,24 +7,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $useremail = $_POST['useremail']; 
     $userpassword = $_POST['userpassword'];
     
-       
-    $hashedPassword= md5($userpassword);
-    $insert = "INSERT INTO users(User_name,User_email,User_password) values('$username','$useremail','$hashedPassword')";
-    $result = mysqli_query($dbconn, $insert);
-
+   
+    $hashedPassword = md5($userpassword);
     
-    if (!$result) {
-         $error_message = 'Error in registration: ' . $dbconn->error;
-        $error_message .= '<br>SQL Query: ' . $insert;
-        die($error_message);
+  
+    $checkEmailQuery = "SELECT * FROM users WHERE User_email = '$useremail'";
+    $checkEmailResult = mysqli_query($dbconn, $checkEmailQuery);
+
+    if (mysqli_num_rows($checkEmailResult) > 0) {
+        echo "<script>alert('The email address is already registered. Please provide a different email address.');</script>";
     } else {
         
-        echo "<script>alert('Your  Registration is successfully done');</script>";
-          header('Location:/xamm-php/GymFit/FitConnect-Manene_Frontend/FitConnect-Corene_frontend/LANDING--PAGE/index.html');
-        exit;
+        $insert = "INSERT INTO users(User_name, User_email, User_password) VALUES('$username', '$useremail', '$hashedPassword')";
+        $result = mysqli_query($dbconn, $insert);
+
+        if (!$result) {
+            $error_message = 'Error in registration: ' . mysqli_error($dbconn);
+            echo "<script>alert('$error_message');</script>";
+        } else {
+            echo "<script>alert('Your registration is successfully done.');</script>";
+            header('Location: /xamm-php/GymFit/FitConnect-Manene_Frontend/FitConnect-Corene_frontend/LANDING--PAGE/index.html');
+            exit;
+        }
     }
 } else {
-
     echo "Invalid request method. Please try again.";
 }
 ?>
