166c575587a682a04528d328218b1ae666a1b93d
Ashrafu <ashraftuyubahe001@gmail.com>
2024-05-18 08:17:31 +0200

    adding  login with google

--- backend/dbconnection.php
+++ backend/dbconnection.php
@@ -2,13 +2,13 @@
 
 
 $host="localhost";
-$name='root';
-$dbname='gymfit';
-$password='123';
+$name="root";
+$dbname="gymfit";
+$password="";
 
 $dbconn= mysqli_connect($host,$name,$password,$dbname);
 
-if($dbconn){
+if(!$dbconn){
  echo "<script>alert('failed to connect  to the database...');</script>";
 }
 
--- backend/userSignup.php
+++ backend/userSignup.php
@@ -1,24 +1,26 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    require 'dbconnection.php';
+
     $username = $_POST['username'];  
     $useremail = $_POST['useremail']; 
     $userpassword = $_POST['userpassword'];
-    require_once 'connection.php';
+    
        
     $hashedPassword= md5($userpassword);
-    $insert = "INSERT INTO users(user_name,user_email,user_password) values('$username','$useremail','$hashedPassword')";
-    $result = mysqli_query($conn, $insert);
+    $insert = "INSERT INTO users(User_name,User_email,User_password) values('$username','$useremail','$hashedPassword')";
+    $result = mysqli_query($dbconn, $insert);
 
     
     if (!$result) {
-         $error_message = 'Error in registration: ' . $conn->error;
+         $error_message = 'Error in registration: ' . $dbconn->error;
         $error_message .= '<br>SQL Query: ' . $insert;
         die($error_message);
     } else {
         
-        echo "<script>alert('Registration is successfully done');</script>";
-        echo "<script>window.location.href = 'index.html';</script>";
+        echo "<script>alert('Your  Registration is successfully done');</script>";
+          header('Location:/xamm-php/GymFit/FitConnect-Manene_Frontend/FitConnect-Corene_frontend/LANDING--PAGE/index.html');
         exit;
     }
 } else {
